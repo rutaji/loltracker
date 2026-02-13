dc76539849009050de85f759b8fe2103af2b7c33
unknown <ondrej.braunsveig@tul.cz>
2026-02-13 19:34:24 +0100

    Templating implemented

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.router import router
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(router)
+app.mount("/static", StaticFiles(directory="static"), name="static")
--- app/endpoints/endpoints.py
+++ app/endpoints/endpoints.py
@@ -1,7 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
+templates = Jinja2Templates(directory="templates")
 
-@router.get("/")
-def read_root():
-    return {"Hello": "World"}
+@router.get("/{text}", response_class=HTMLResponse)
+async def read_root(request: Request, text : str):
+    return templates.TemplateResponse("index.html", {"request": request, "text": text})
