8971a75a7132ca206b09ce0aeafe0b0d3054e4b1
Martin Čížek <officialpatera6@gmail.com>
2026-03-10 11:18:51 +0100

    fixed syntax errors, pathing and added jinja template

--- app/endpoints/endpoints.py
+++ app/endpoints/endpoints.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from services.summonerServices import get_summoner_service
-import services.stubDAO
+from app.services.summonerServices import get_summoner_service
+from app.services.stubDAO import stubDAO
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="app/templates")
 dao=stubDAO()
 
 @router.get("/{text}", response_class=HTMLResponse)
@@ -15,4 +15,13 @@ async def read_root(request: Request, text : str):
 
 @router.get("/summoner/{name}", response_class=HTMLResponse)
 async def get_summoner(request: Request, name: str):
-    summoner=get_summoner_service(name, dao)
+    data=get_summoner_service(name, dao)
+
+    return templates.TemplateResponse(
+        "summoner.html",
+        {
+            "request": request,
+            "summoner": data["summoner"],
+            "matchData": data["matchData"]
+        }
+    )
