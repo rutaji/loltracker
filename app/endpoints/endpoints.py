f2a575b5366f0173869acb505d6940df1ef3f6df
OndrejBraunsveig <ondrej.braunsveig@tul.cz>
2026-03-12 19:09:43 +0100

    Search bar implementation, tagline added to services and models

--- app/endpoints/endpoints.py
+++ app/endpoints/endpoints.py
@@ -1,34 +1,105 @@
-from fastapi import APIRouter, Request
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.staticfiles import StaticFiles
+from urllib.parse import quote, unquote
+
+from fastapi import APIRouter, Form, Request
+from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.encoders import jsonable_encoder
-from app.services.summonerServices import get_summoner_service
+from app.services.summonerServices import get_summoner_service, get_matches_service
 from app.services.stubDAO import stubDAO
+from app.models.summonerModels import SummonerData
 
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 dao=stubDAO()
 
-@router.get("/{text}", response_class=HTMLResponse)
-async def read_root(request: Request, text : str):
-    return templates.TemplateResponse("index.html", {"request": request, "text": text})
+@router.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
-@router.get("/summoner/{name}", response_class=HTMLResponse)
-async def get_summoner(request: Request, name: str, offset: int = 0, ajax: bool = False):
+@router.post("/", response_class=RedirectResponse)
+async def search(
+    summoner_name: str = Form(default=""),
+    summoner_tagline: str = Form(default=""),
+    champion_name: str = Form(default=""),
+):
+    summoner_name = summoner_name.strip()
+    summoner_tagline = summoner_tagline.strip()
+    champion_name = champion_name.strip()
+
+    # Summoner path
+    if summoner_name:
+        # Neni nutny checkovat tagline pac je required v html
+        summoner = get_summoner_service(summoner_name, summoner_tagline, dao)
+        if summoner:
+            return RedirectResponse(
+                url=(
+                    f"/summoner/{quote(summoner_name, safe="")}"
+                    f"/{quote(summoner_tagline, safe="")}"
+                ),
+                status_code=303,
+            )
+        return RedirectResponse(
+            url=(
+                f"/summoner/not-found?name={quote(summoner_name, safe="")}"
+                f"&tagline={quote(summoner_tagline, safe="")}"
+            ),
+            status_code=303
+        )
+
+    # Champion path
+    '''
+    if champion_name:
+        champion = get_champion_service()
+        if champion:
+            return RedirectResponse(
+                url=f"/champion/{quote(champion_name, safe='')}",
+                status_code=303,
+            )
+    '''
+
+    return RedirectResponse(
+        url=f"/",
+        status_code=303
+    )
+
+@router.get("/summoner/{name}/{tagline}", response_class=HTMLResponse)
+async def get_summoner(request: Request, name: str, tagline: str, offset: int = 0, ajax: bool = False):
 
     count=3
 
-    data=get_summoner_service(name, dao, offset, count)
+    summoner = get_summoner_service(name, tagline, dao)
+    if summoner is None:
+        return RedirectResponse(
+            url=(
+                f"/summoner/not-found?name={quote(name, safe="")}"
+                f"&tagline={quote(tagline, safe="")}"
+            ),
+            status_code=303
+        )
+
+    match_page = get_matches_service(name, tagline, offset, count, dao)
+    summoner_data = SummonerData(summoner=summoner, matchPage=match_page)
 
     if ajax:
-        return JSONResponse(jsonable_encoder(data["matchData"]))
+        return summoner_data
 
     return templates.TemplateResponse(
         "summoner.html",
         {
             "request": request,
-            "summoner": data["summoner"],
-            "matchData": data["matchData"]
+            "summoner": summoner,
+            "matchData": match_page
         }
+    )
+
+@router.get("/summoner/not-found")
+async def summoner_not_found(request: Request, name: str = "", tagline: str = ""):
+
+    return templates.TemplateResponse(
+        request=request,
+        name="summoner_not_found.html",
+        context={
+            "searched_name": name,
+            "searched_tagline": tagline,
+        },
     )
